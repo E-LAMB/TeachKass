05eb9cfc7605e2167980d809cf0bcf641ec9adc1
E-LAMB <113103562+E-LAMB@users.noreply.github.com>
2023-12-28 21:47:17 +0000

    Adding player stuff

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public int number = 5; // Integers
+    public float den = 4.5f; // Floats / Decimals
+
+    public bool condition = true; // Booleans
+
+    public string words = "hello world"; // Strings
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
